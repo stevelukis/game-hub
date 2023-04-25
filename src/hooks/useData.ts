1ab1d64b4d35c4c1159b302e646a5def17ae0976
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-04-25 08:19:50 +0800

    Refactor useData to use react query

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,37 +1,25 @@
 import { AxiosRequestConfig, CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
+import { useQuery } from "@tanstack/react-query";
+
+const useData = <T>(
+  queryKey: any[],
+  endpoint: string,
+  requestConfig?: AxiosRequestConfig
+) => {
+  return useQuery<FetchResponse<T>, Error>({
+    queryKey: queryKey,
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<T>>(endpoint, { ...requestConfig })
+        .then((res) => res.data),
+  });
+};
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
-
-const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
-  const [data, setData] = useState<T[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<FetchResponse<T>>(endpoint, { signal: controller.signal, ...requestConfig })
-      .then((res) => {
-        setData(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message)
-        setLoading(false);
-      });
-
-    return () => controller.abort();
-  }, deps ? [...deps] : []);
-
-  return { data, error, isLoading };
-};
 
 export default useData;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
@@ -18,17 +17,13 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
-      },
+  useData<Game>(["games", gameQuery], "/games", {
+    params: {
+      genres: gameQuery.genre?.id,
+      platforms: gameQuery.platform?.id,
+      ordering: gameQuery.sortOrder,
+      search: gameQuery.searchText,
     },
-    [gameQuery]
-  );
+  });
 
 export default useGames;
